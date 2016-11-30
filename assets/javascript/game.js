566125a052e8d8994dc0032d5fba13b61ed9455b
jlp0328 <jlp0328@gmail.com>
2016-11-30 14:27:02 -0500

    Final HW Upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,55 @@
-var attempts = 10;
-var wins = 0;
-var losses = 0;
+
 var words =["mean girls", "clueless", "wedding crashers", "old school"];
 var randomWord = words[Math.floor(Math.random() * words.length)].split("");
 var displayBlank = [];
 for (var i = 0; i < randomWord.length; i++){
   displayBlank.push("_");
 }
-var remainingLetters = randomWord.length;
-  console.log(randomWord);
 
+var attempts = randomWord.length;
+var wins = 0;
 
-
-
-// var displayBlank = [];
-// var wordBlank = new Array (randomWord.length);
-// for (var i = 0; i < wordBlank.length; i++){
-//   displayBlank = wordBlank.join(" _ ");
-// }
-// console.log(wordBlank);
-
+document.getElementById("tries").innerHTML = attempts;
 
 document.getElementById("randomWordGenerated").innerHTML = displayBlank.join(" ");
-console.log(displayBlank);
+document.getElementById("correctGuesses").innerHTML = "Times Guessed Correctly: "+ wins;
+
+    console.log(randomWord);
+    console.log(displayBlank);
 
 var lettersGuessed = [];
-
-for (var j = 0; j < randomWord.length; j++){
 
 document.onkeyup = function (event){
    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
    lettersGuessed.push(userGuess);
+   document.getElementById("firstGuess").innerHTML = lettersGuessed.join(" ");
+   console.log(lettersGuessed);
 
-    document.getElementById("firstGuess").innerHTML = lettersGuessed.join(" ");
-
-    console.log(lettersGuessed);
-
-    // if(randomWord[j] === userGuess){
-   if (randomWord.indexOf(userGuess) > -1){
-        displayBlank[j] = userGuess;
-        alert("Correct!");
-        document.getElementById("randomWordGenerated").innerHTML = displayBlank.join(" ");
-
-   }
-   else{
-      alert("Wrong!");
       attempts--;
-      document.getElementById("counter").innerHTML = attempts;
+      console.log(attempts);
+
+// If variable guessed is in the random word, then replace character in array and print to screen.
+  for (var j = 0; j < randomWord.length; j++){
+
+    if(userGuess === randomWord[j]){
+      displayBlank[j] = randomWord[j];
+      wins++;
+      console.log("wins");
+      console.log("displayBlank is " + displayBlank);
+      console.log("userGuess is " + userGuess);
+      document.getElementById("tries").innerHTML = attempts;
+      document.getElementById("randomWordGenerated").innerHTML = displayBlank.join(" ");
+      document.getElementById("correctGuesses").innerHTML = "Times Guessed Correctly: "+ wins;
+    }
+
+     else{
+      document.getElementById("tries").innerHTML = attempts;
    }
 
 }
 }
 
-
-
-   // guess = prompt("What is your guess?");
-
-
-//   document.getElementById("randomWordGenerated").innerHTML = lettersGuessed;
-
-
-// // var lettersGuessed = [];
-
-
-//     for (var i = 1; i < randomWord.length; i++) {
-
-
-//     document.getElementById("guessedLetters").innerHTML = userGuess;
-//     console.log(userGuess);
-//     console.log(guess);
-
-
+//Game needs to end when the maximum guesses have been reached OR the word has been guessed.
 
 
 
